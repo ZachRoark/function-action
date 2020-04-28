e50774e633812b730cea17d0cd816728c58087f3
Zach Roark <zach.roark@yahoo.com>
2020-04-28 14:55:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function contactFactory(contactData) {
+    let contact = {
+      name: null,
+      title: null,
+      email: null
+    }
+  
+    for (let i=0; i < contactData.length; i++) {
+       ///add each arry item to contact
+     }
+  
+    return contact
+  }
+  
+  function createContact(name, title, email) {
+    let newContact = /// pass name, title, email to contactFactory as an array
+    console.table(Contact)
+  }
